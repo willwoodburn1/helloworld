b7b3d81c734f4a5e49fa357a0e844dd074f974e9
User <user@Users-MacBook-Pro.local>
2020-12-02 11:54:53 +1000

    initial import

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send(`Example app listening at http://localhost:${port}`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
